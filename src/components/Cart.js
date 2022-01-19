3aec41c8b2a70ee478253368049be918223b1063
mynameisnikhil <84395668+mynameisnikhil@users.noreply.github.com>
2022-01-19 15:28:50 +0530

    Create Cart.js

---
+++ src/components/Cart.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./header.css";
+
+
+export const Cart = () => {
+    return (
+        <div className="navbar-content">
+            <img className="shopping-cart-img" src="/shopping-cart.svg" alt="cart" />
+        </div>
+    );
+};
